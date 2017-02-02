67eaaf744829fd2d57a535e48d52727fa6f82602
Sergey Gusev <matveyboyko@gmail.com>
2017-02-02 23:12:31 +0200

    added drawing up of an order

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -9,6 +9,8 @@
 namespace app\controllers;
 use app\models\Product;
 use app\models\Cart;
+use app\models\Order;
+use app\models\OrderItems;
 use Yii;
 
 
@@ -17,11 +19,61 @@ class CartController extends AppController
     public function actionAdd()
     {
         $id = Yii::$app->request->get('id');
-        $products = Product::findOne($id);
-        if(empty($products)) return false;
+        $qty = (int)Yii::$app->request->get('qty');
+        $qty = !$qty ? 1 : $qty;
+        $product = Product::findOne($id);
+        if(empty($product)) return false;
         $session = Yii::$app->session;
         $session->open();
         $cart = new Cart();
-        $cart->addToCart($products);
+        $cart->addToCart($product, $qty);
+        if(!Yii::$app->request->isAjax)
+            return $this->redirect(Yii::$app->request->referrer);
+        $this->layout = false;
+        return $this->render('cart-modal', compact('session'));
     }
+
+    public function actionClear()
+    {
+        $session = Yii::$app->session;
+        $session->open();
+        $session->remove('cart');
+        $session->remove('cart.qty');
+        $session->remove('cart.sum');
+        $this->layout = false;
+        return $this->render('cart-modal', compact('session'));
+    }
+
+    public function actionDelItem()
+    {
+        $id = Yii::$app->request->get('id');
+        $session = Yii::$app->session;
+        $session->open();
+        $cart = new Cart();
+        $cart->clear($id);
+        $this->layout = false;
+        return $this->render('cart-modal', compact('session'));
+    }
+
+    public function actionShow()
+    {
+        $session = Yii::$app->session;
+        $session->open();
+        $this->layout = false;
+        return $this->render('cart-modal', compact('session'));
+    }
+    
+    public function actionView()
+    {
+        $session = Yii::$app->session;
+        $session->open();
+        $this->setMeta('Корзина');
+        $order = new Order();
+        if($order->load(Yii::$app->request->post()))
+        {
+            debug(Yii::$app->request->post());
+        }
+        return $this->render('view', compact('session', 'order'));
+    }
+
 }
