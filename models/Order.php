f9cc1e0dc749d1cb4ca8aa087ad75f90134cbd9e
Sergey Gusev <matveyboyko@gmail.com>
2017-02-03 23:01:45 +0200

    added drawing up of an order

--- models/Order.php
+++ models/Order.php
@@ -4,6 +4,8 @@ namespace app\models;
 
 use Yii;
 use yii\db\ActiveRecord;
+use yii\behaviors\TimestampBehavior;
+use yii\db\Expression;
 
 /**
  * This is the model class for table "order".
@@ -27,6 +29,21 @@ class Order extends ActiveRecord
     public static function tableName()
     {
         return 'order';
+    }
+
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => TimestampBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+                ],
+                // если вместо метки времени UNIX используется datetime:
+                'value' => new Expression('NOW()'),
+            ],
+        ];
     }
 
     public function getOrderItems()
